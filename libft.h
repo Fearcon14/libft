a15eb88e94a4747959c51ec0287eb29e2ec1c368
Kevin Sinn <ksinn@2-E-8.42heilbronn.de>
2024-10-09 14:41:44 +0200

    added ft_strncmp.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:28:00 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/09 13:17:23 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/09 14:27:42 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,6 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
