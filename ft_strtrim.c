d584217288cf8439f0c62bf634c860cfe165c53d
Kevin Sinn <ksinn@1-C-21.42heilbronn.de>
2024-10-10 15:50:38 +0200

    fixed all issues by removing previous 'fixes' before fixing the major issues

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,31 +6,25 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:30:57 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/10 15:35:05 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/10 15:46:51 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// #include <stdio.h>
-
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*res;
-	size_t	s1_len;
 	size_t	left;
 	size_t	right;
 	size_t	i;
 
-	s1_len = ft_strlen(s1);
 	left = 0;
 	right = 0;
 	while (s1[left] && ft_strchr(set, s1[left]))
 		left++;
-	// printf("left: %zu\n", left);
-	if (s1_len > 0)
-		right = s1_len - 1;
-	// printf("right: %zu\n", right);
+	if (ft_strlen(s1) > 0)
+		right = ft_strlen(s1) - 1;
 	while (right > left && ft_strchr(set, s1[right]))
 		right--;
 	res = (char *)malloc((right - left + 2) * sizeof(char));
@@ -42,15 +36,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		res[i] = s1[i + left];
 		i++;
 	}
-	if (left == right)
-		i--;
 	res[i] = '\0';
 	return (res);
 }
-
-// int	main(void)
-// {
-// 	char	s1[] = "";
-
-// 	ft_strtrim(s1, "");
-// }
