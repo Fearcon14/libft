22717491e74d7724727cb3e820b3ee92c1c9dc17
Kevin Sinn <ksinn@1-D-27.42heilbronn.de>
2024-10-10 12:01:51 +0200

    changed logic to garanty coniguously

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 10:35:38 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/10 10:57:58 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/10 12:01:20 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,8 @@
 
 void	*calloc(size_t count, size_t size)
 {
-	char	**obj;
-	size_t	i;
+	char	*obj;
 
-	obj = (char **)malloc(count * sizeof(char *));
+	obj = (char *)malloc(count * size * sizeof(char));
 	// TODO add malloc protection
-	i = 0;
-	while (i < count)
-	{
-		obj[i] = (char *)malloc(size);
-		// TODO add malloc protection and free previous mallocs
-		ft_bzero(obj[i], size);
-		i++;
-	}
 }
