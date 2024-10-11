a8ac3f69298529fb491ff0cc733b95e5c4271fa1
Kevin Sinn <ksinn@1-E-14.42heilbronn.de>
2024-10-11 13:11:44 +0200

    removed extra variable as it is not necessary

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 12:50:30 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/11 13:06:27 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/11 13:11:05 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-	t_list	*last;
-
 	if (!lst)
 		return (NULL);
-	last = lst;
-	while (last->next)
-		last = last->next;
-	return (last);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
