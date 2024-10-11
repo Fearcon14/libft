be90f9099c6d431804210abf0d020d6929bfd7df
Kevin Sinn <ksinn@1-E-14.42heilbronn.de>
2024-10-11 12:50:13 +0200

    solved ft_lstsize.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:28:00 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/11 12:40:34 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/11 12:49:19 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
+int					ft_lstsize(t_list *lst);
 
 #endif
