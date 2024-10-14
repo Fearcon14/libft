822f04bca115782de973d21900f9ffee8bd21acd
Kevin Sinn <ksinn@1-E-12.42heilbronn.de>
2024-10-14 13:47:40 +0200

    added descriptors for functions

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,12 +6,17 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 15:07:16 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/11 15:55:34 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/14 13:45:20 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+Deletes and frees the given node and every successor of that node,
+using the function ’del’ and free(3).
+Finally, the pointer to the list must be set to NULL
+*/
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*curr;
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,12 +6,16 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 16:05:14 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/11 16:15:43 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/14 13:46:06 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+Iterates the list ’lst’ and applies the function ’f’
+on the content of each node
+*/
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
 	if (!lst || !(*f))
