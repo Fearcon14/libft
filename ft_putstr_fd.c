ffc723ce12837b266f105d4972d19fd88d235be4
Kevin Sinn <ksinn@1-C-9.42heilbronn.de>
2024-10-16 18:42:53 +0200

    updated libft

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: ksinn <ksinn@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 18:32:03 by ksinn             #+#    #+#             */
-/*   Updated: 2024/10/11 14:57:56 by ksinn            ###   ########.fr       */
+/*   Updated: 2024/10/15 14:52:49 by ksinn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 /*
 Outputs the string ’s’ to the given file descriptor
 */
-void	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, s, ft_strlen(s));
+	return (write(fd, s, ft_strlen(s)));
 }
